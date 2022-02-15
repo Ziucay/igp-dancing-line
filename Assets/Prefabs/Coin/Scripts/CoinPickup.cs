e48df24b61814fbdd429d5bb62d69eaba484ef3f
Azat Bariev <justlumajustmail@gmail.com>
2022-02-15 23:45:02 +0300

    Add counter, coin animation

--- Assets/Prefabs/Coin/Scripts/CoinPickup.cs
+++ Assets/Prefabs/Coin/Scripts/CoinPickup.cs
@@ -1,14 +1,15 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
+    public delegate void CoinPickupAction();
+    public static event CoinPickupAction OnPicked;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            if (OnPicked != null)
+                OnPicked();
             Destroy(gameObject);
         }
     }
